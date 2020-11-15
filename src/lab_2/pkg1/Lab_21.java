344b1cfb9b34fe678fd012efa18cc01398025898
User <User@home-pc>
2020-11-15 20:43:25 +0700

    Java Lab

---
+++ src/lab_2/pkg1/Lab_21.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_2.pkg1;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("а=1, c=a+1 m=2, n=3 решение уравнений");
+        System.out.println("b=6, d=b*2-1 p=4, g=5 нахождение корней");
+    }
+    
+}
